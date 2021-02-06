fda1a17a18de9007a898f008138081c442b85ab6
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2021-02-06 12:32:23 +0900

    test deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+app.use(express.json()); // body-parser
+
+const http = require("http");
+const server = http.createServer(app);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+const userRouter = require("./router/userRouter");
+
+app.use("/api/user", userRouter);
